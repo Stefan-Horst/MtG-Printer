4669ec232270ec619e596658e45a8333ffc7c3fe
Stefan <stefan-horst@gmx.de>
2026-04-22 03:44:24 +0200

    simplify and improve names of mprof scripts

--- mprof_eval_scripts/load_cards_into_db.py
+++ mprof_eval_scripts/load_cards_into_db.py
@@ -5,10 +5,10 @@ from webserver.load_scryfall_data import load_scryfall_card_data_chunks
 create_database("cards.db", "webserver/create_db.sql")
 db = DatabaseManager("cards.db")
 for card_data in load_scryfall_card_data_chunks("data.json"):
-    if ((card_data.get("layout") in ["art_series", "scheme", "vanguard", "planar", "double_faced_token"]) 
-        or card_data.get("border_color") == "silver" 
+    if ((card_data["layout"] in ["art_series", "scheme", "vanguard", "planar", "double_faced_token"]) 
+        or card_data["border_color"] == "silver" 
         or card_data.get("security_stamp") == "acorn"
-        or card_data.get("set_type") in ["memorabilia", "minigame", "alchemy"]):
+        or card_data["set_type"] in ["memorabilia", "minigame", "alchemy"]):
         continue  # Skip abnormal cards that are not relevant for printing
     try:
         db.save_card_data(card_data, commit=False)
