f63894b764e1c1240735f3e4c0c2a2751037412e
Stefan <stefan-horst@gmx.de>
2026-04-24 18:45:34 +0200

    use asyncio for downloading images script

--- download_images.py
+++ download_images.py
@@ -1,37 +1,58 @@
-import threading
+import asyncio
 import timeit
-from webserver.load_scryfall_data import load_scryfall_card_data_chunks, get_card_image_urls, download_multiple_card_images
+from io import BytesIO
+import aiohttp
+from PIL import Image
+from webserver.load_scryfall_data import load_scryfall_card_data_chunks, get_card_image_urls, SCRYFALL_HEADERS, TIMEOUT, IMAGE_DIR
 
 
-counter = 0
-threads = []
+async def download_all_images():
+    counter = 0
+    data = []
+    for card_data in load_scryfall_card_data_chunks("data.json"):
+        if ((card_data["layout"] in ["art_series", "scheme", "vanguard", "planar", "double_faced_token"]) 
+            or card_data["border_color"] == "silver" 
+            or card_data.get("security_stamp", "") == "acorn"
+            or card_data["set_type"] in ["memorabilia", "minigame", "alchemy"]
+            or card_data.get("digital", False) == True
+            or "legal" not in card_data["legalities"].values()):
+            continue  # Skip abnormal cards that are not relevant for printing
+        image_uris = get_card_image_urls(card_data)
+        data.extend(image_uris)
+        
+        counter += 1
+        if counter == 1000:
+            break
 
-start_time = timeit.default_timer()
+    def _make_filename_valid(name: str) -> str:
+        return name.replace("/", "_").replace('"', "").replace("?", "").replace(":", "").strip()
 
-for card_data in load_scryfall_card_data_chunks("data.json"):
-    if ((card_data["layout"] in ["art_series", "scheme", "vanguard", "planar", "double_faced_token"]) 
-        or card_data["border_color"] == "silver" 
-        or card_data.get("security_stamp", "") == "acorn"
-        or card_data["set_type"] in ["memorabilia", "minigame", "alchemy"]
-        or card_data.get("digital", False) == True
-        or "legal" not in card_data["legalities"].values()):
-        continue  # Skip abnormal cards that are not relevant for printing
-    image_uris = get_card_image_urls(card_data)
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_image(url, _make_filename_valid(name), session) for name, url in data]
+        await asyncio.gather(*tasks)
 
-    thread = threading.Thread(target=download_multiple_card_images, args=(image_uris,))
-    threads.append(thread)
-    thread.start()
+async def download_image(url, name, session):
+    async with session.get(url, headers=SCRYFALL_HEADERS, timeout=TIMEOUT, raise_for_status=True) as response:
+        try:
+            content = await response.read()
+        except Exception:
+            print(f"Failed to download image for {name}. Trying again...")
+            await asyncio.sleep(100 / 1000)
+            try:
+                async with session.get(url, headers=SCRYFALL_HEADERS, timeout=TIMEOUT, raise_for_status=True) as response:
+                    content = await response.read()
+            except Exception as e:
+                print(f"Failed to download image for {name} again: {e}")
+                return
+    image = Image.open(BytesIO(content))
+    image.save(f"{IMAGE_DIR}/{name}.jpg")
+
+async def main():
+    start_time = timeit.default_timer()
     
-    if len(threads) >= 100:  # Limit the number of concurrent threads to avoid overwhelming the system
-        threads[0].join()  # Wait for the first thread to finish before starting a new one
-        threads.pop(0)
-    
-    counter += 1
-    if counter == 1000:
-        break
-    
-for thread in threads:
-    thread.join()
+    await download_all_images()
 
-end_time = timeit.default_timer()
-print(f"Downloaded images for {counter} cards in {end_time - start_time:.4f} seconds")
+    end_time = timeit.default_timer()
+    print(f"Downloaded in {end_time - start_time:.4f} seconds")
+
+asyncio.run(main())
