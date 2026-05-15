c56ff2f5f4c039c8c733f63e9d779f9b259d9871
Stefan <stefan-horst@gmx.de>
2026-05-15 05:54:45 +0200

    fix and improve function param values in main

--- src/main.py
+++ src/main.py
@@ -18,16 +18,16 @@ def main():
 
     # Step 2: Create a SQLite database and load card data into it; save image URLs for later downloading
     print("=> Loading card data into database...")
-    create_database("cards.db", "webserver/create_db.sql", ignore_if_exists=True)
-    db = DatabaseManager("cards.db")
+    create_database(ignore_if_exists=True)
+    db = DatabaseManager()
     file_image_data = []
     for card_data in load_scryfall_card_data_chunks():
         try:
-            db.save_card_data(card_data, commit=False)
+            db.save_card_data(card_data, commit=False, handle_exist="ignore")
         except Exception:
             print(f"Failed to save card data for {card_data.get('name', 'Unknown')}. Trying again...")
             try:
-                db.save_card_data(card_data, commit=False)
+                db.save_card_data(card_data, commit=False, handle_exist="ignore")
             except Exception as e:
                 print(f"Failed to save card data for {card_data.get('name', 'Unknown')}: {e}")
         image_urls = get_card_image_urls(card_data)
@@ -46,12 +46,12 @@ def main():
     
     # Step 3: Download card images based on the downloaded card data
     print("=> Downloading card images...")
-    failed_downloads = download_multiple_card_images(file_image_data)
+    failed_downloads = download_multiple_card_images(file_image_data, skip_existing=True)
     print(f"Finished downloading images. {len(failed_downloads)} failed downloads.")
     for i in range(IMAGE_DOWNLOAD_RETRIES):
         if failed_downloads:
             print(f"Retrying failed downloads (attempt {i + 1}/{IMAGE_DOWNLOAD_RETRIES})...")
-            failed_downloads = download_multiple_card_images(failed_downloads)
+            failed_downloads = download_multiple_card_images(failed_downloads, skip_existing=True)
             print(f"Retry finished. {len(failed_downloads)} failed downloads remain.")
         else:
             print("All images downloaded successfully.")
@@ -62,11 +62,11 @@ def main():
     # Step 4: Process downloaded images (turn into high-contrast black & white versions)
     print("=> Processing card images...")
     try:
-        process_all_images()
+        process_all_images(skip_existing=True)
     except Exception:
         print("Failed to process images. Trying again...")
         try:
-            process_all_images()
+            process_all_images(skip_existing=True)
         except Exception as e:
             print(f"Failed to process images: {e}\nExiting.")
             return
