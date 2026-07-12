2f40e902eba621a4b3e51b01e51e7021a66e99b3
Stefan <stefan-horst@gmx.de>
2026-07-12 23:08:47 +0200

    add printer image preprocessing

--- src/raspi_io/printer.py
+++ src/raspi_io/printer.py
@@ -1,9 +1,8 @@
 from PIL.Image import Image
-from escpos.printer import Serial
+from escpos.printer import Serial, Dummy
 
 
-DEVICE_WIDTH = 384 # width of the printer in pixels
-CUT_MARGIN = 2 # margin to leave at the bottom of the image for cutting
+CUT_MARGIN = 3 # margin to leave at the bottom of the image for cutting
 
 
 class PrinterManager:
@@ -12,20 +11,29 @@ class PrinterManager:
     
     def __init__(self):
         self.printer = Serial(devfile="/dev/serial0", baudrate=9600, profile="POS-5890")
+        self.dummy_printer = Dummy(profile="POS-5890")
+        # Width of the printer in pixels and the possible number of characters per line for normal and narrow font
+        self.device_width = self.printer.profile["media"]["width"]["pixels"]
+        self.base_cols_per_line = self.printer.profile["fonts"]["0"]["columns"]
+        self.narrow_cols_per_line = self.printer.profile["fonts"]["1"]["columns"]
 
     def is_printer_online(self) -> bool:
         """Check if the printer is online and ready to receive commands."""
         return self.printer.is_online()
 
-    def print_card_image(self, image: Image) -> None:
+    def print_card_image(self, image: Image, feed_after_image: bool = True) -> None:
         """Print a card image on the thermal printer. The image must already be processed 
-        to be high-contrast black & white and resized to fit the printer width.
+        to be high-contrast black & white and resized to fit the printer width. 
+        Preprocesses the image using a dummy printer for faster actual printing.
         
         Args:
             image: PIL Image object to be printed
+            feed_after_image: Whether to feed the printer after printing the image so it can be cut properly
         """
-        self.printer.image(image, impl="bitImageColumn", center=True)
-        self.printer.print_and_feed(CUT_MARGIN)
+        self.dummy_printer.image(image, impl="bitImageColumn", center=True)
+        self.printer._raw(self.dummy_printer.output)
+        if feed_after_image:
+            self.printer.print_and_feed(CUT_MARGIN)
     
     def close(self) -> None:
         self.printer.close()
