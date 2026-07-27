346b8a3f6de4fc7b15b4ba26c0fbb685ef8475b7
Stefan <stefan-horst@gmx.de>
2026-07-27 05:36:29 +0200

    fix accidental use of float mana values

--- src/card_handling/queries.py
+++ src/card_handling/queries.py
@@ -90,7 +90,7 @@ def get_mana_cost_range(db: DatabaseManager) -> tuple[int, int]:
     result = db.execute_query(
         "SELECT MIN(cmc), MAX(cmc) FROM cards WHERE type_line LIKE '%Creature%'"
     )
-    return result[0][0], result[0][1]
+    return int(result[0][0]), int(result[0][1])
 
 def get_nonexistent_creature_mana_costs(db: DatabaseManager) -> list[int]:    
     """Get a list of all creature mana costs that do not exist in the database. 
@@ -104,8 +104,8 @@ def get_nonexistent_creature_mana_costs(db: DatabaseManager) -> list[int]:
     result = db.execute_query(
         "SELECT DISTINCT cmc FROM cards WHERE type_line LIKE '%Creature%'"
     )
-    mana_costs = [r[0] for r in result]
-    return [cost for cost in range(int(mana_costs[-1])+1) if cost not in mana_costs]
+    mana_costs = [int(r[0]) for r in result]
+    return [cost for cost in range(mana_costs[-1] + 1) if cost not in mana_costs]
 
 def _get_standardized_card_dict(card_info: dict, relevant_face: str = None) -> dict:
     """Get a dictionary containing the standardized card information, meaning all relevant keys exist. 
